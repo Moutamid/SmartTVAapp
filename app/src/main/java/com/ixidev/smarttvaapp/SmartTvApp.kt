8eb4cee6b2a1903db998ecc9358b47a4eab74f9c
suleman81 <sunnybajwa904@gmail.com>
2023-08-12 18:44:51 +0500

    Initial commit

---
+++ app/src/main/java/com/ixidev/smarttvaapp/SmartTvApp.kt
@@ -0,0 +1,12 @@
+package com.ixidev.smarttvaapp
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by ABDELMAJID ID ALI on 31/10/2020.
+ * Email : abdelmajid.idali@gmail.com
+ * Github : https://github.com/ixiDev
+ */
+@HiltAndroidApp
+class SmartTvApp : MultiDexApplication()
